4a87cf93e783b1c2518cbbeea9ff666b3b7f908c
Gabriel Claudino <gaclaudino.13@gmail.com>
2024-07-14 00:09:19 -0300

    wip: integrar fitting_functions.py na adesao_view.py mas esta dando erro na biblioteca do reliability

--- controller/adesao_view.py
+++ controller/adesao_view.py
@@ -1,5 +1,11 @@
 from PyQt5 import uic, QtWidgets, QtGui
 import os
+#import numpy as np
+#from scipy.stats import norm
+#import matplotlib.pyplot as plt
+#from reliability.Fitters import Fit_Weibull_2P, Fit_Lognormal_2P
+#from utilities.fitting_functions import compare_distributions
+
 
 class AdhesionView:
     def __init__(self, manager, model):
@@ -27,7 +33,7 @@ class AdhesionView:
             print('Modelo recebido na AdhesionView com os seguintes dados:')
             print(self.model)
             self.print_model_data()
-            self.gerar_grafico()
+            #self.gerar_grafico()
         # Botão de voltar para tela anterior: register_view
         self.adhesion_view.btnBack.clicked.connect(self.btn_voltar)
         # Botão de Sair
@@ -51,7 +57,23 @@ class AdhesionView:
         self.manager.show_register_view(window_title=self.metadata.get('project_name', 'Project name'), metadata=self.metadata)
 
     def gerar_grafico(self):
-        print("Gerando gráfico...")
+        tef_data, _ = self.get_data_from_model()  # Asumindo que tef_data é o que você precisa para a análise
+        dist, best_distribution = compare_distributions(tef_data, "_time_unit_") # return dist, best_distribution
+        print(f"Melhor distribuição: {best_distribution}")
+        # Plot aqui ou defina um método para lidar com plotagem em outra parte do código
+
     
+    def get_data_from_model(self):
+        tef_data = []
+        tr_data = []
+        rows = self.model.rowCount()
+        for row in range(rows):
+            tef_item = self.model.item(row, 0)
+            tr_item = self.model.item(row, 1)
+            if tef_item and tr_item:
+                tef_data.append(float(tef_item.text()))
+                tr_data.append(float(tr_item.text()))
+        return np.array(tef_data), np.array(tr_data)
+
     def close(self):
         self.adhesion_view.close()
